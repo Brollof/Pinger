00f384e99a69f7a834dca4fa654d761f9a9aadd9
Brollof <thefish3r@gmail.com>
2020-04-28 20:42:17 +0200

    app icon changed; resources names improved

--- Pinger/resVersion.h
+++ Pinger/resVersion.h
@@ -1,6 +1,11 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by Pinger.rc
+//
+#define VERSION_REVISION                0
+#define VER_VER_DEBUG                   0
+#define VERSION_MAJOR                   1
+#define VERSION_MINOR                   1
 
 // Next default values for new objects
 // 
